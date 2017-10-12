b17d5ef420378796705f27a0df33b467cb1a6dae
scosacescu <stephanie.cosacescu@gmail.com>
2017-10-12 14:49:30 -0700

    Job 5 - Help wanted page features

--- application/config/autoload.php
+++ application/config/autoload.php
@@ -58,7 +58,8 @@ $autoload['packages'] = array();
 |
 |	$autoload['libraries'] = array('user_agent' => 'ua');
 */
-$autoload['libraries'] = array('parser');
+$autoload['libraries'] = array('parser', 'parsedown');
+
 
 /*
 | -------------------------------------------------------------------
