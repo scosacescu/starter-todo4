483db93039bb1a94bcf299385c9a98198d94b235
scosacescu <stephanie.cosacescu@gmail.com>
2017-10-19 14:18:57 -0700

    Job 9: Role specific maintenance list - for Owner only

--- application/controllers/Mtce.php
+++ application/controllers/Mtce.php
@@ -1,62 +1,68 @@
 <?php
 
-class Mtce extends Application
-{
+class Mtce extends Application {
 
-  private $items_per_page = 10;
+    private $items_per_page = 10;
 
-  public function index()
-  {
-    $this->page(1);
-  }
+    public function index() {
+        $this->page(1);
+    }
 
 // Show a single page of todo items
-  private function show_page($tasks)
-  {
-    $role = $this->session->userdata('userrole');
-    $this->data['pagetitle'] = 'TODO List Maintenance ('. $role . ')';
-    // build the task presentation output
-    $result = ''; // start with an empty array
-    foreach ($tasks as $task) {
-      if (!empty($task->status))
-        $task->status = $this->app->status($task->status);
-      $result .= $this->parser->parse('oneitem', (array)$task, true);
+    private function show_page($tasks) {
+        $role = $this->session->userdata('userrole');
+        $this->data['pagetitle'] = 'TODO List Maintenance (' . $role . ')';
+        // build the task presentation output
+        $result = ''; // start with an empty array
+        foreach ($tasks as $task) {
+            if (!empty($task->status))
+                $task->status = $this->app->status($task->status);
+// INSERT the next three lines. The fourth is already there
+            if ($role == ROLE_OWNER)
+                $result .= $this->parser->parse('oneitemx', (array) $task, true);
+            else
+                $result .= $this->parser->parse('oneitem', (array) $task, true);
+        }
+        $this->data['display_tasks'] = $result;
+
+        // and then pass them on
+        $this->data['pagebody'] = 'itemlist';
+        $this->render();
     }
-    $this->data['display_tasks'] = $result;
 
-    // and then pass them on
-    $this->data['pagebody'] = 'itemlist';
-    $this->render();
-  }
-
-  private function pagenav($num) {
-    $lastpage = ceil($this->tasks->size() / $this->items_per_page);
-    $parms = array(
-      'first' => 1,
-      'previous' => (max($num-1,1)),
-      'next' => min($num+1,$lastpage),
-      'last' => $lastpage
-    );
-    return $this->parser->parse('itemnav',$parms,true);
-  }
-
-  function page($num = 1)
-  {
-    $records = $this->tasks->all(); // get all the tasks
-    $tasks = array(); // start with an empty extract
-
-    // use a foreach loop, because the record indices may not be sequential
-    $index = 0; // where are we in the tasks list
-    $count = 0; // how many items have we added to the extract
-    $start = ($num - 1) * $this->items_per_page;
-    foreach ($records as $task) {
-      if ($index++ >= $start) {
-        $tasks[] = $task;
-        $count++;
-      }
-      if ($count >= $this->items_per_page) break;
+    private function pagenav($num) {
+        $lastpage = ceil($this->tasks->size() / $this->items_per_page);
+        $parms = array(
+            'first' => 1,
+            'previous' => (max($num - 1, 1)),
+            'next' => min($num + 1, $lastpage),
+            'last' => $lastpage
+        );
+        return $this->parser->parse('itemnav', $parms, true);
     }
-    $this->data['pagination'] = $this->pagenav($num);
-    $this->show_page($tasks);
-  }
+
+    function page($num = 1) {
+        $records = $this->tasks->all(); // get all the tasks
+        $tasks = array(); // start with an empty extract
+        // use a foreach loop, because the record indices may not be sequential
+        $index = 0; // where are we in the tasks list
+        $count = 0; // how many items have we added to the extract
+        $start = ($num - 1) * $this->items_per_page;
+        foreach ($records as $task) {
+            if ($index++ >= $start) {
+                $tasks[] = $task;
+                $count++;
+            }
+            if ($count >= $this->items_per_page)
+                break;
+        }
+        $this->data['pagination'] = $this->pagenav($num);
+
+        // INSERT next three lines
+        $role = $this->session->userdata('userrole');
+        if ($role == ROLE_OWNER)
+            $this->data['pagination'] .= $this->parser->parse('itemadd', [], true);
+        $this->show_page($tasks);
+    }
+
 }
