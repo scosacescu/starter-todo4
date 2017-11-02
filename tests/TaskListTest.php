d4f845e51fc62ae522798fdafb92a41decfd20c1
haro-freezd <harmanmahal@gmail.com>
2017-11-02 11:56:41 -0700

    Added TaskList comparison test

---
+++ tests/TaskListTest.php
@@ -0,0 +1,31 @@
+<?php
+
+class TaskListTest extends PHPUnit_Framework_TestCase
+{
+  private $CI;
+
+  public function setUp()
+  {
+    // Load CI instance normally
+    $this->CI = &get_instance();
+  }
+
+  public function testTaskCompletion()
+  {
+    $tasks = $this->CI->tasks->all();
+    $unfinishedTasks = 0;
+    $finishedTasks = 0;
+
+    foreach ($tasks as $task) {
+      if ($task->status != 2)
+        $unfinishedTasks++;
+    }
+
+    foreach ($tasks as $task) {
+      if ($task->status == 2)
+        $finishedTasks++;
+    }
+
+    $this->assertGreaterThan($finishedTasks, $unfinishedTasks);
+  }
+}
